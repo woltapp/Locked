602181e8ad0bae7f7b544763fde1d49df8862d73
Dag Ågren <dag.agren@wolt.com>
2024-04-24 14:25:31 +0300

    Turn on strict concurrency.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,10 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Locked"
+            name: "Locked",
+            swiftSettings: [
+			    .enableExperimentalFeature("StrictConcurrency")
+  			]
         )
     ]
 )
